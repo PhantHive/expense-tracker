3e3667b5c5f0a9c194032bf5c1a4172a5d5a11d6
PhantHive <phantomhive72@yahoo.fr>
2024-10-28 14:22:32 +0100

    build: website deploy gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-    /* config options here */
-};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    basePath: process.env.NODE_ENV === 'production' ? '/expense-tracker' : '',
+}
 
-export default nextConfig;
+module.exports = nextConfig
